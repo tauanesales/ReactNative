334979ad3c7e8234bb91a23668330f6830baebda
Tauane Sales <taysales6@gmail.com>
2022-05-03 09:44:33 -0300

    implements multi-component creation

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import  {Text } from 'react-native';
+import  { View} from 'react-native';
+import {Comp1, Comp2} from './componentes/Multi';
 
+// function App(){
+//     return <Text> A vida é bela </Text>
+//     <View><Comp1></Comp1></View>
+// }
 
-function App(){
-    return <Text>First Component </Text>
-}
-
-export default App
+export default () => (
+    <View>
+        <Comp1></Comp1>
+        <Comp2></Comp2>
+    </View>
+)
---
+++ exercicios/src/componentes/Multi.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Text } from "react-native"
+
+function Comp() {
+   return <Text>Primeira Frase</Text>
+}
+
+export function Comp1() {
+    return <Text>Segunda Frase</Text>
+}
+
+export function Comp2() {
+    return <Text>Terceira Frase</Text>
+ }
