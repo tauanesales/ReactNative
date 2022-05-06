a4e17cbd6fa32f5d2dd29b97fa29e03c33cc3e5e
Tauane Sales <taysales6@gmail.com>
2022-05-06 14:39:34 -0300

    fixing login issues

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -5,6 +5,7 @@ import Botao from './componentes/botao';
 import Plataforma from './componentes/plataforma'
 import RefatorandoPlataforma from './componentes/refatorandoPlataforma';
 import EntradaUsuario from './componentes/entradaUsuario'
+import EntradaSenha from './componentes/entradaSenha'
 // function App(){
 //     return <Text> A vida é bela </Text>
 //     <View><Comp1></Comp1></View>
@@ -16,6 +17,7 @@ export default () => (
         <Comp1></Comp1> 
         <Comp2></Comp2>
         <EntradaUsuario></EntradaUsuario>
+        <EntradaSenha></EntradaSenha>
         <Botao></Botao>
         {/* <RefatorandoPlataforma></RefatorandoPlataforma> */}
     </View>
@@ -26,7 +28,8 @@ const style = StyleSheet.create({
         flexGrow :1,
         justifyContent: "center",
         alignItems:"center",
-        padding:20,
+        padding:10,
         color:'while',
+
     }
 })
--- exercicios/src/componentes/entradaSenha.js
+++ exercicios/src/componentes/entradaSenha.js
@@ -4,14 +4,15 @@ import estilo from "./estilo"
 
 
 export default props => {
-    const [email, setEmail ] = useState ('annne@ibm.com')
+    const [senha, setSenha] = useState ('1234')
 
  
     return(
         <View>
-            <TextInput  placeholder="Digite seu email IBM"
-            value={email}
-            onChangeText={email => setEmail(email)}/>
+
+            <TextInput placeholder="Digite sua senha"
+            value={senha}
+            onChangeText={senha => setSenha(senha)}/>
         </View>
     )
 } 
