ad7cc419e0d23054bdca8c85d73789b407b848aa
Tauane Sales <taysales6@gmail.com>
2022-05-06 13:54:10 -0300

    implementing email and password typing

--- exercicios/src/componentes/entradaUsuario.js
+++ exercicios/src/componentes/entradaUsuario.js
@@ -1,13 +1,22 @@
-import React from "react"
-import { Text, TextInput, View } from "react-native"
+import React, { useState } from "react"
+import { Text, TextInput, View} from "react-native"
 import estilo from "./estilo"
 
 
 export default props => {
+    const [email, setEmail ] = useState ('annne@ibm.com')
+    const [senha,setSenha] = useState ('1234')
+
+ 
     return(
         <View>
-            <TextInput  placeholder="Digite seu email IBM"/>
-            <TextInput placeholder="Digite sua senha"/>
+            <TextInput  placeholder="Digite seu email IBM"
+            value={email}
+            onChangeText={email => setEmail(email)}/>
+
+            <TextInput placeholder="Digite sua senha"
+            value={senha}
+            onChangeText={senha => setSenha(email)}/>
         </View>
     )
 } 
