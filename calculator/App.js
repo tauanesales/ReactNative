b1828b0c4e7b6cabf81b421c6f9309635e39ee60
Tauane Sales <taysales6@gmail.com>
2022-05-10 14:18:04 -0300

    implement style of buttons and imports

--- calculator/App.js
+++ calculator/App.js
@@ -7,19 +7,43 @@ import Button from './src/components.js/Button';
 //     <View><Comp1></Comp1></View>
 // }
 
-export default class App extends Component {
-  render() {
+export default () => {
+ 
     return (
-      <View style={styles.conteiner}>
-       <View style={styles.conteiner}>
-      </View>
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <Button label='AC'/>
+          <Button label='/'/>
+          <Button label='7'/>
+          <Button label='8'/>
+          <Button label='9'/>
+          <Button label='*'/>
+          <Button label='S4'/>
+          <Button label='5'/>
+          <Button label='6'/>
+          <Button label='-'/>
+          <Button label='1'/>
+          <Button label='2'/>
+          <Button label='3'/>
+          <Button label='+'/>
+          <Button label='0'/>
+          <Button label='.'/>
+          <Button label='='/>
+
+        </View>
       </View>
     );
   }
 
 const styles = StyleSheet.create({
-    conteiner:{
+    container:{
       flex:1,
     }, 
-    flex :'row',
+    buttons: {
+      flexDirection:'row',
+      flexWrap: 'wrap'
+
+    }
 });
+
+
